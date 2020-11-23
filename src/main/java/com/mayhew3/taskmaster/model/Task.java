e9143b1e295ab457d214940f6695d91d9837cb08
mayhew3 <commander.boffo@gmail.com>
2020-11-22 18:18:26 -0800

    TM-122: Unrestrict my fields

--- src/main/java/com/mayhew3/taskmaster/model/Task.java
+++ src/main/java/com/mayhew3/taskmaster/model/Task.java
@@ -11,8 +11,8 @@ public class Task extends RetireableDataObject {
   public FieldValueString project = registerStringField("project", Nullability.NULLABLE);
   public FieldValueString context = registerStringField("context", Nullability.NULLABLE);
 
-  public FieldValueInteger urgency = registerIntegerField("urgency", Nullability.NOT_NULL);
-  public FieldValueInteger priority = registerIntegerField("priority", Nullability.NOT_NULL);
+  public FieldValueInteger urgency = registerIntegerField("urgency", Nullability.NULLABLE);
+  public FieldValueInteger priority = registerIntegerField("priority", Nullability.NULLABLE);
 
   public FieldValueInteger duration = registerIntegerField("duration", Nullability.NULLABLE);
 
@@ -22,7 +22,7 @@ public class Task extends RetireableDataObject {
   public FieldValueTimestamp completionDate = registerTimestampField("completion_date", Nullability.NULLABLE);
   public FieldValueTimestamp urgentDate = registerTimestampField("urgent_date", Nullability.NULLABLE);
 
-  public FieldValueInteger gamePoints = registerIntegerField("game_points", Nullability.NOT_NULL);
+  public FieldValueInteger gamePoints = registerIntegerField("game_points", Nullability.NULLABLE);
 
   public FieldValueInteger recurNumber = registerIntegerField("recur_number", Nullability.NULLABLE);
   public FieldValueString recurUnit = registerStringField("recur_unit", Nullability.NULLABLE);
